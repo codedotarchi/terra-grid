58732efcf3c71336bb5ef15db6dc5b70241d9748
Noah Pitts <noahpitts@code.archi>
2019-03-02 18:48:42 -0800

    added a bunch of packages

---
+++ js/like_button.js
@@ -0,0 +1,23 @@
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+// ... the starter code you pasted ...
+
+const domContainer = document.querySelector('#INPUT_container');
+ReactDOM.render(e(LikeButton), domContainer);
